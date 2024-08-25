98e339993236606151927b7945e647242c5a3cdd
Samuel Langarica <142830126+samuellangarica@users.noreply.github.com>
2024-08-25 15:57:48 -0600

    change interval from 1s to 2s

--- src/scheduler/scheduler.service.ts
+++ src/scheduler/scheduler.service.ts
@@ -4,8 +4,8 @@ import { Cron, CronExpression, Interval } from '@nestjs/schedule';
 @Injectable()
 export class SchedulerService {
   // This job will run every 10 seconds
-  @Interval(1000)
+  @Interval(2000)
   handleInterval() {
-    console.log('Task running every 1 second');
+    console.log('Task running every 2 second');
   }
 }
